1d52560290980f3dc2181c6a2050bbbef0407293
Devesh Shukla <devshoe@Deveshs-MacBook-Air.local>
2024-04-01 14:11:13 +0530

    base config

---
+++ lua/mappings.lua
@@ -0,0 +1,41 @@
+require "nvchad.mappings"
+
+-- add yours here
+
+local map = vim.keymap.set
+
+vim.api.nvim_del_keymap('n','<Tab>')
+vim.api.nvim_del_keymap('n', '<S-Tab>')
+
+
+map("n", "<leader><tab>", function()
+  require("nvchad.tabufline").next()
+end, { desc = "Buffer Goto next" })
+
+map("n", "<leader><S-tab>", function()
+  require("nvchad.tabufline").prev()
+end, { desc = "Buffer Goto prev" })
+
+map("n", "<leader>c$", "<cmd>set autochdir<CR><cmd>set noautochdir<CR>", {desc = "Set working dir to folder of current buffer"})
+map("n", "<leader>c_", "<cmd>cd ..<CR>", { desc = "Set working dir to one above"})
+
+map("n", "<C-d>", "<C-d>zz", {desc = "Down half page"})
+map("n", "<C-u>", "<C-u>zz", {desc = "Up half page"})
+
+map("n", "<leader>fs", "<cmd>Telescope lsp_dynamic_workspace_symbols<CR>", {desc = "Search for symbols in current workspace"})
+
+
+map("n", "<leader>co", "<cmd>ChatGPT<CR>", {desc = "Open ChatGPT"})
+map("n", "<leader>cx", function ()
+   local status = vim.fn.execute('Copilot status')
+   if string.find(status, "Ready") then
+        -- If Copilot is ready, disable it
+        vim.cmd('Copilot disable')
+        print('Copilot disabled')
+    else
+        -- If Copilot is not ready, enable it
+        vim.cmd('Copilot enable')
+        print('Copilot enabled')
+    end
+end, {desc = "Toggle Github Copilot"})
+
---
+++ lua/plugins/init.lua
@@ -0,0 +1,64 @@
+return {
+  {
+    'github/copilot.vim',
+    lazy = false,
+
+  },
+
+  {
+    'nvim-tree/nvim-web-devicons',
+    lazy = false,
+  },
+
+  {
+    "stevearc/conform.nvim",
+    event = 'BufWritePre', -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+
+  {
+  	"williamboman/mason.nvim",
+  	opts = {
+  		ensure_installed = {
+  			"lua-language-server", "stylua",
+  			"html-lsp", "css-lsp" , "prettier",
+        "gopls",
+  		},
+  	},
+  },
+
+  {
+  	"nvim-treesitter/nvim-treesitter",
+  	opts = {
+  		ensure_installed = {
+  			"vim", "lua", "vimdoc",
+       "html", "css"
+  		},
+  	},
+  },
+
+  {
+  "jackMort/ChatGPT.nvim",
+    event = "VeryLazy",
+    config = function()
+      local opts = require('configs.chatgpt')
+      require("chatgpt").setup(opts)
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "folke/trouble.nvim",
+      "nvim-telescope/telescope.nvim"
+    }
+  },
+}
